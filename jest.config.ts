20d4a3d020b1e5bf1a9d86e91cd7961fc3024241
Ignatius Bagus <ignatius.mbs@gmail.com>
2021-01-10 00:51:01 +0700

    feat: add ts-node and use jest ts config

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,8 @@
-module.exports = {
+import type { Config } from '@jest/types';
+
+export default async (): Promise<Config.InitialOptions> => ({
 	transform: { '^.+\\.ts?$': 'ts-jest' },
 	testEnvironment: 'node',
 	testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
 	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-};
+});
