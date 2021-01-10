33da28af7850665b5c1764121b29987a5d92d341
Ignatius Bagus <ignatius.mbs@gmail.com>
2021-01-10 15:02:55 +0700

    add utils correctness and perf tests

---
+++ tests/correctness/utils.spec.ts
@@ -0,0 +1,10 @@
+import * as utils from '../../utils';
+
+describe('capitalize', () => {
+	it('change one letter for one word', () => {
+		expect(utils.capitalize('hello')).toBe('Hello');
+	});
+	it('change two letter for two words', () => {
+		expect(utils.capitalize('hello world')).toBe('Hello World');
+	});
+});
