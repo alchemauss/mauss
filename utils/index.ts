e79f687feb797a1509d863bc726d045040c4266c
Ignatius Bagus <ignatius.mbs@gmail.com>
2021-01-20 14:40:42 +0700

    refactor: remove type casting and return keyword

--- utils/index.ts
+++ utils/index.ts
@@ -1,9 +1,6 @@
-export function capitalize(text: string, lower?: boolean): string {
-	return (lower ? text.toLowerCase() : text).replace(/(?:^|\s)\S/g, (a) => a.toUpperCase());
-}
-
-export const checkNum = <T>(text: T): T | number =>
-	isNaN(text as any) ? text : parseInt(text as any);
+export const capitalize = (text: string, lower?: boolean): string =>
+	(lower ? text.toLowerCase() : text).replace(/(?:^|\s)\S/g, (a) => a.toUpperCase());
+export const checkNum = <T>(text: T): T | number => (Number.isNaN(+text) ? text : +text);
 
 export const random = {
 	int(max: number, min = 0): number {
