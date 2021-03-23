b4008f83b3d48aa2de00ab051da4aff7a60f2864
Ignatius Bagus <ignatius.mbs@gmail.com>
2021-03-23 16:36:36 +0700

    binary search initial stab

--- src/bits/find.ts
+++ src/bits/find.ts
@@ -1,3 +1,23 @@
+export function binary<T>(
+	array: T[],
+	check: {
+		item: (item: T) => false | ((item: T) => any);
+		pointer: (item: T) => boolean;
+	}
+) {
+	let start = 0, end = array.length - 1; // prettier-ignore
+	while (start <= end) {
+		const midpoint = (start + end) >> 1;
+		const current = array[midpoint];
+		const passes = check.item(current);
+		if (passes) return passes(current);
+		if (check.pointer(current)) {
+			end = midpoint - 1;
+		} else start = midpoint + 1;
+	}
+	return null;
+}
+
 export function minmax(array: number[]): [number, number] {
 	if (!array.length) return [0, 0];
 
