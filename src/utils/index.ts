5906e8c669bf8d084641509fef2011188a471c49
Ignatius Bagus <ignatius.mbs@gmail.com>
2021-03-26 17:13:50 +0700

    feat(utils): more random things (#12)
    * feat: add random float and refactor int to use float
    
    * feat: add random ipv4
    
    * feat: add random boolean
    
    * feat: add random array of numbers
    
    * feat: add random hex

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,9 +3,25 @@ export const capitalize = (text: string, lower?: boolean): string =>
 export const checkNum = <T>(text: T): T | number => (Number.isNaN(+text) ? text : +text);
 
 export const random = {
-	int(max: number, min = 0): number {
+	array(length: number, max: number, min = 0) {
+		return Array.from({ length }, () => this.int(max, min));
+	},
+	bool(): boolean {
+		return !!this.int();
+	},
+	float(max = 1, min = 0): number {
+		return Math.random() * (max - min) + min;
+	},
+	hex(): string {
+		const code = ~~(this.float() * (1 << 24));
+		return `#${code.toString(16)}`;
+	},
+	int(max = 1, min = 0): number {
 		[min, max] = [Math.ceil(min), Math.floor(max)];
-		return Math.floor(Math.random() * (max - min)) + min;
+		return Math.floor(this.float(max, min));
+	},
+	ipv4(): string {
+		return [0, 1, 2, 3].map((i) => this.int(255) + (!i ? 1 : 0)).join('.');
 	},
 	key<T>(dict: Record<string, T>): string {
 		const keys = Object.keys(dict);
