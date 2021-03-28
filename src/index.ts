0ce3c6f7cd74d8041856d4155dc46038e3969fd8
Ignatius Bagus <ignatius.mbs@gmail.com>
2021-03-28 12:16:40 +0700

    feat: change cookies to function

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,29 @@
-export const cookies = {
-	create(key: string, value: string | number | boolean = '', validDays = 0): void {
-		let expiry = '';
-		if (validDays) {
-			const date = new Date();
-			date.setTime(date.getTime() + validDays * 24 * 60 * 60 * 1e3);
-			expiry = `; expires=${date.toUTCString()}`;
-		}
-		document.cookie = `${key}=${value}${expiry}; path=/;`;
-	},
-	get(key: string): string {
-		const name = `${key}=`;
-		for (const cookie of document.cookie.split(';')) {
-			if (!cookie.trim().startsWith(name)) continue;
-			return cookie.trim().slice(name.length);
-		}
-		return '';
-	},
-	remove(key: string): void {
-		document.cookie = `${key}=; expires=Thu, 01 Jan 1970 00:00:01 GMT;`;
-	},
-};
+export function cookies(cookie = (typeof window !== 'undefined' && document.cookie) || '') {
+	const browser = typeof window !== 'undefined' && !!document.cookie;
+	return {
+		create(key: string, value: string | number = '', validDays = 0): void {
+			let expiry = '';
+			if (validDays) {
+				const date = new Date();
+				date.setTime(date.getTime() + validDays * 24 * 60 * 60 * 1e3);
+				expiry = `; expires=${date.toUTCString()}`;
+			}
+			cookie = `${key}=${value}${expiry}; path=/;`;
+		},
+		get(key: string): string {
+			const name = `${key}=`;
+			for (const c of cookie.split(';')) {
+				if (!c.trim().startsWith(name)) continue;
+				return c.trim().slice(name.length);
+			}
+			return '';
+		},
+		remove(key: string): Error | void {
+			if (!browser) return new Error('Not in browser, cannot remove');
+			cookie = `${key}=; expires=Thu, 01 Jan 1970 00:00:01 GMT;`;
+		},
+	};
+}
 
 export function debounce(fn: any, time = 300) {
 	let timeout: NodeJS.Timeout;
