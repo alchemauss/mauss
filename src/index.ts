2d3dd0f77c2bce91ddb62c1fef8412fa99cdfd51
Ignatius Bagus <ignatius.mbs@gmail.com>
2021-04-11 00:10:14 +0700

    refactor: cookies parsing (#13)
    * feat: change cookies to function
    
    * feat: rework get logic to only loop once
    
    * fix: throw Error instead of returning

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,32 @@
-export const cookies = {
-	create(key: string, value: string | number | boolean = '', validDays = 0): void {
-		let expiry = '';
-		if (validDays) {
-			const date = new Date();
-			date.setTime(date.getTime() + validDays * 24 * 60 * 60 * 1e3);
-			expiry = `; expires=${date.toUTCString()}`;
-		}
-		document.cookie = `${key}=${value}${expiry}; path=/;`;
-	},
-	get(key: string): string {
-		const name = `${key}=`;
-		for (const cookie of document.cookie.split(';')) {
-			if (!cookie.trim().startsWith(name)) continue;
-			return cookie.trim().slice(name.length);
-		}
-		return '';
-	},
-	remove(key: string): void {
-		document.cookie = `${key}=; expires=Thu, 01 Jan 1970 00:00:01 GMT;`;
-	},
-};
+export function cookies(cookie = (typeof window !== 'undefined' && document.cookie) || '') {
+	const browser = typeof window !== 'undefined' && !!document.cookie;
+	return {
+		create(key: string, value: string | number = '', validDays = 0): void {
+			let expiry = '';
+			if (validDays) {
+				const date = new Date();
+				date.setTime(date.getTime() + validDays * 24 * 60 * 60 * 1e3);
+				expiry = `; expires=${date.toUTCString()}`;
+			}
+			cookie = `${key}=${value}${expiry}; path=/;`;
+		},
+		get(key: string): string {
+			for (let i = 0, c = 0; i < cookie.length; i++) {
+				if (c === key.length && cookie[i + 1] === '=') {
+					let end = i + 2;
+					while (cookie[end++] !== ';');
+					return cookie.slice(i + 1, end);
+				}
+				c = key[c] === cookie[i] ? c + 1 : 0;
+			}
+			return '';
+		},
+		remove(key: string): void {
+			if (!browser) throw new Error('Not in browser, cannot remove');
+			cookie = `${key}=; expires=Thu, 01 Jan 1970 00:00:01 GMT;`;
+		},
+	};
+}
 
 export function debounce(fn: any, time = 300) {
 	let timeout: NodeJS.Timeout;
