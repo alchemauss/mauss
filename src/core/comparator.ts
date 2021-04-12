699cd982f4dd6c1f15d07a991b4af707a780f703
Ignatius Bagus <ignatius.mbs@gmail.com>
2021-04-12 20:33:03 +0700

    feat: enhance comparator even more

--- src/core/comparator.ts
+++ src/core/comparator.ts
@@ -1,14 +1,17 @@
 export const compare: Record<string, any> & {
 	format: { [type: string]: RegExp };
+	boolean: (x: boolean, y: boolean) => number;
+	number: (x: number, y: number) => number;
 	date: (x: string, y: string) => number;
 	string: (x: string, y: string) => number;
 } = {
 	format: { date: /\d{1,4}-\d{1,2}-\d{1,4}/ },
+	boolean: (x, y) => +y - +x,
+	number: (x, y) => y - x,
 	date: (x, y) => new Date(y).getTime() - new Date(x).getTime(),
 	string(x, y) {
-		for (const [type, exp] of Object.entries(this.format)) {
+		for (const [type, exp] of Object.entries(this.format))
 			if (exp.test(x) && exp.test(y)) return this[type](x, y);
-		}
 		return x.localeCompare(y);
 	},
 };
@@ -17,10 +20,13 @@ export function comparator(x: Record<string, any>, y: Record<string, any>): numb
 	const common = [...new Set([...Object.keys(x), ...Object.keys(y)])].filter(
 		(k) => k in x && k in y && typeof x[k] === typeof y[k] && x[k] !== y[k]
 	);
-	for (let i = 0, key = common[i]; i < common.length; key = common[++i]) {
-		if (x[key] === null && y[key] === null) continue;
-		if (typeof x[key] === 'string') return compare.string(x[key], y[key]);
-		if (typeof x[key] === 'object') return comparator(x[key], y[key]);
+	for (
+		let i = 0, key = common[i], data = typeof x[key];
+		i < common.length && x[key] !== null && y[key] !== null;
+		key = common[++i], data = typeof x[key]
+	) {
+		if (data === 'object') return comparator(x[key], y[key]);
+		if (data in compare) return compare[data](x[key], y[key]);
 	}
 	return 0;
 }
