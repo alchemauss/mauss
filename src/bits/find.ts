db49659470d608ad4b2ca602082f4b03afe3ef5a
Ignatius Bagus <ignatius.mbs@gmail.com>
2021-04-19 12:52:59 +0700

    refactor and rename variables

--- src/bits/find.ts
+++ src/bits/find.ts
@@ -1,19 +1,19 @@
 export function binary<T>(
-	array: T[],
+	sorted: T[],
 	check: {
 		item: (item: T) => false | ((item: T) => any);
 		pointer: (item: T) => boolean;
 	}
 ): T | undefined {
-	let start = 0, end = array.length - 1; // prettier-ignore
-	while (start <= end) {
-		const midpoint = (start + end) >> 1;
-		const current = array[midpoint];
+	let start = 0, final = sorted.length - 1; // prettier-ignore
+	while (start <= final) {
+		const midpoint = (start + final) >> 1;
+		const current = sorted[midpoint];
 		const passes = check.item(current);
 		if (passes) return passes(current);
-		if (check.pointer(current)) {
-			end = midpoint - 1;
-		} else start = midpoint + 1;
+		const flag = check.pointer(current);
+		start = flag ? start : midpoint + 1;
+		final = flag ? midpoint - 1 : final;
 	}
 	return;
 }
