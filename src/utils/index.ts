23cb1f0b05e05018fb00adfe9e11b1855fff11be
Ignatius Bagus <ignatius.mbs@gmail.com>
2021-04-23 18:13:46 +0700

    change checkNum to tryNumber

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,4 +3,4 @@ export { random } from './toolbox';
 export const capitalize = (text: string, normalize?: boolean): string =>
 	(normalize ? text.toLowerCase() : text).replace(/(?:^|\s)\S/g, (a) => a.toUpperCase());
 
-export const checkNum = <T>(text: T): T | number => (Number.isNaN(+text) ? text : +text);
+export const tryNumber = <T>(text: T): T | number => (Number.isNaN(+text) ? text : +text);
