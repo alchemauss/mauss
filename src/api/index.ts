6a2853a779d30ea960f5f9c30d0872dd2ea31140
Ignatius Bagus <ignatius.mbs@gmail.com>
2021-05-30 19:30:13 +0700

    feat(api): enhance send return value

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,15 +1,11 @@
-type InitOpts = { host?: string; check?: (path: string) => string };
-type SendParams = { method: string; path: string; data?: BodyInit; token?: string };
-type MaussFetch = (url: string, init?: RequestInit) => Promise<Response>;
-
 const tryImport = async (browser: boolean) => {
 	if (browser) return window.fetch;
+	if (typeof fetch !== 'undefined') return fetch;
 	try {
 		const module = await import('node-fetch');
 		return <MaussFetch>(module.default as any);
 	} catch (err) {
-		console.warn(`Cannot use in server, "node-fetch" not available`);
-		return '';
+		return 'Cannot use fetch, "node-fetch" not available';
 	}
 };
 
@@ -18,12 +14,12 @@ async function send({ method, path, data, token }: SendParams) {
 	const browser = typeof window !== 'undefined';
 	if (!options.fetch) {
 		const module = await tryImport(browser);
-		if (typeof module !== 'string') options.fetch = module;
-		else if (!browser) return module;
+		if (typeof module === 'string') throw new Error(module);
+		options.fetch = module;
 	}
 
 	const opts: RequestInit = { method };
-	opts.headers = {}; // Workaround TypeScript not correctly implying if put above
+	opts.headers = {}; // TS workaround for "Object is possibly 'undefined'."
 	if (browser && typeof FormData !== 'undefined' && data instanceof FormData) {
 		opts.body = data;
 	} else if (data) {
@@ -48,12 +44,9 @@ async function send({ method, path, data, token }: SendParams) {
 		`${base || 'localhost:3000'}/${path}`;
 
 	const res = await fetch(url, opts);
-	const text = await res.text();
-	try {
-		return JSON.parse(text);
-	} catch (err) {
-		return text;
-	}
+	const body = await (res.ok ? res.json() : res.text());
+	const err = res.ok ? undefined : new Error(body);
+	return { res, body, err };
 }
 
 export const init = async ({ host, check }: InitOpts) => {
@@ -61,21 +54,27 @@ export const init = async ({ host, check }: InitOpts) => {
 	options.host = host && host.startsWith('http') ? host : undefined;
 	options.check = typeof check === 'function' ? check : undefined;
 	const module = await tryImport(browser);
-	if (typeof module !== 'string') options.fetch = module;
+	if (typeof module === 'string') console.warn(module);
+	else options.fetch = module;
 };
 
-export function get(path: string, token?: string): Promise<Response> {
+export function get(path: string, token?: string): APIResponse {
 	return send({ method: 'GET', path, token });
 }
 
-export function del(path: string, token?: string): Promise<Response> {
+export function del(path: string, token?: string): APIResponse {
 	return send({ method: 'DELETE', path, token });
 }
 
-export function post(path: string, data: any, token?: string): Promise<Response> {
+export function post(path: string, data: any, token?: string): APIResponse {
 	return send({ method: 'POST', path, data, token });
 }
 
-export function put(path: string, data: any, token?: string): Promise<Response> {
+export function put(path: string, data: any, token?: string): APIResponse {
 	return send({ method: 'PUT', path, data, token });
 }
+
+type InitOpts = { host?: string; check?: (path: string) => string };
+type SendParams = { method: string; path: string; data?: BodyInit; token?: string };
+type MaussFetch = (url: string, init?: RequestInit) => Promise<Response>;
+type APIResponse = Promise<{ res: Response; body: any; err?: Error }>;
