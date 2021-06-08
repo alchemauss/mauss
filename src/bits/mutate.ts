af7a85cdd4cd7b886b69d0feae03063482c2bc1f
Ignatius Bagus <ignatius.mbs@gmail.com>
2021-06-08 18:52:40 +0700

    add explicit void return and export default

--- src/bits/mutate.ts
+++ src/bits/mutate.ts
@@ -1,6 +1,10 @@
-export function shuffle<T>(arr: T[]) {
+export function shuffle<T>(arr: T[]): void {
 	for (let i = arr.length - 1; i > 0; i--) {
 		const j = Math.floor(Math.random() * (i + 1));
 		[arr[i], arr[j]] = [arr[j], arr[i]];
 	}
 }
+
+export default {
+	shuffle,
+};
