1055cea48c3ef682b1b98dbe8ed51278af4ae178
Ignatius Bagus <ignatius.mbs@gmail.com>
2021-06-08 17:47:33 +0700

    change function to void

--- src/bits/mutate.ts
+++ src/bits/mutate.ts
@@ -1,7 +1,6 @@
-export function shuffle<T>(arr: T[]): T[] {
+export function shuffle<T>(arr: T[]) {
 	for (let i = arr.length - 1; i > 0; i--) {
 		const j = Math.floor(Math.random() * (i + 1));
 		[arr[i], arr[j]] = [arr[j], arr[i]];
 	}
-	return arr;
 }
