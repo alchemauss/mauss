d5d42901d256a2f373cb45d149e6d9dbfeebb9aa
Ignatius Bagus <ignatius.mbs@gmail.com>
2021-07-24 15:49:17 +0700

    add Filter helper typings

--- src/typings/helper.ts
+++ src/typings/helper.ts
@@ -1,2 +1,3 @@
+export type Filter<T, Validator> = T extends Validator ? T : never;
 export type Overwrite<T, K> = Omit<T, keyof K> & K;
 export type PickByValue<T, V> = Pick<T, { [K in keyof T]: T[K] extends V ? K : never }[keyof T]>;
