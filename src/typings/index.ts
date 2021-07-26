2b55faa92763c59c5c44a85239cc96336a9218af
Ignatius Bagus <ignatius.mbs@gmail.com>
2021-07-26 20:47:40 +0700

    add NonEmptyArray helper typings

--- src/typings/index.ts
+++ src/typings/index.ts
@@ -1,3 +1,3 @@
 export type { Entries, Typify } from './enhancer';
-export type { Filter, Overwrite, PickByValue } from './helper';
+export type { Filter, NonEmptyArray, Overwrite, PickByValue } from './helper';
 export type { Join, Permutation, Split } from './operation';
