531a571a19cc71f9aa47b53c3971e509da9b627c
Ignatius Bagus <ignatius.mbs@gmail.com>
2021-09-05 08:15:41 +0700

    add Strict type enhancer

--- src/typings/enhancer.ts
+++ src/typings/enhancer.ts
@@ -1,1 +1,4 @@
+export type JSONState<T> = { [P in keyof T]: T[P] extends { toJSON: () => infer J } ? J : T[P] };
+export type Overwrite<A, B> = Omit<A, keyof B> & B;
+export type Strict<T> = { [P in keyof T as {} extends Record<P, any> ? never : P]: T[P] };
 export type Typify<T> = { [P in keyof T]: Typify<T[P]> };
--- src/typings/helper.ts
+++
@@ -1,2 +0,0 @@
-export type Overwrite<A, B> = Omit<A, keyof B> & B;
-export type JSONState<T> = { [P in keyof T]: T[P] extends { toJSON: () => infer J } ? J : T[P] };
