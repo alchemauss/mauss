6ab5f3b36171372f62431e1ef02cfd7d36ac3b61
Ignatius Bagus <ignatius.mbs@gmail.com>
2021-10-15 18:26:39 +0700

    loosen value types

--- src/web/index.ts
+++ src/web/index.ts
@@ -5,11 +5,11 @@ import * as cookies from './cookies';
  * @param bound object with key-value pair to be updated in the URL
  * @returns final pathname with query string
  */
-export function qpm(bound: Record<string, string>): string {
+export function qpm(bound: Record<string, string | number | boolean>): string {
 	if (typeof window === 'undefined') return '';
 	const kvs = Object.entries(bound).reduce((a, [k, v]) => {
-		v = v.trim();
-		if (!v) return a;
+		if (typeof v === 'string') v = v.trim();
+		if (typeof v === 'string' && !v) return a;
 		if (!a) a = '?';
 		if (a !== '?') a += '&';
 		return `${a}${k}=${encodeURIComponent(v)}`;
