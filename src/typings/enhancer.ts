4720f35d55443a8f93ef39961fda6734866115e1
Ignatius Bagus <ignatius.mbs@gmail.com>
2021-11-01 18:12:03 +0700

    chore: simplify typings module (#42)

--- src/typings/enhancer.ts
+++
@@ -1,7 +0,0 @@
-import type { PickByValue } from './helper';
-
-export type Entries<T> = Array<{ [K in keyof T]: [keyof PickByValue<T, T[K]>, T[K]] }[keyof T]>;
-export type Filter<T, Validator> = T extends Validator ? T : never;
-export type Overwrite<A, B> = Omit<A, keyof B> & B;
-export type Strict<T> = { [P in keyof T as {} extends Record<P, any> ? never : P]: T[P] };
-export type Typify<T> = { [P in keyof T]: Typify<T[P]> };
