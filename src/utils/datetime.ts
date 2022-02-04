2967136d746aafe276d845eac9a38c551b3cea5a
Ignatius Bagus <ignatius.mbs@gmail.com>
2022-02-04 17:25:25 +0700

    placeholder

---
+++ src/utils/datetime.ts
@@ -0,0 +1,3 @@
+export const date = {};
+
+export const time = {};
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,5 @@
-import { random } from './toolbox';
-
-export { random };
+export { date, time } from './datetime';
+export { random } from './toolbox';
 
 export const capitalize = (text: string, normalize?: boolean): string =>
 	(normalize ? text.toLowerCase() : text).replace(/(?:^|\s)\S/g, (a) => a.toUpperCase());
