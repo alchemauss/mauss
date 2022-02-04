94a22af77c84dfcdbaed2b5dcbd205e54678663a
Ignatius Bagus <ignatius.mbs@gmail.com>
2022-02-04 17:29:09 +0700

    double bangs are checking truthy

--- src/guards/index.ts
+++ src/guards/index.ts
@@ -1,2 +1,2 @@
-export const isExists = <T>(i: T | 0 | '' | false | null | undefined | typeof NaN): i is T => !!i;
+export const truthy = <T>(i: T | 0 | '' | false | null | undefined | typeof NaN): i is T => !!i;
 export const notNullish = <T>(i: T | null | undefined): i is T => i !== null && i !== undefined;
