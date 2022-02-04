e00dadc09b9efff3bc805dcd6cc49821bf78df66
Ignatius Bagus <ignatius.mbs@gmail.com>
2022-02-04 17:09:49 +0700

    feat: add `uuid` method to random

--- src/utils/toolbox.ts
+++ src/utils/toolbox.ts
@@ -26,4 +26,11 @@ export const random = {
 	ipv4(): string {
 		return [0, 1, 2, 3].map((i) => this.int(255) + (!i ? 1 : 0)).join('.');
 	},
+
+	/** unsafe and for fast prototyping only, use with caution for production */
+	uuid(): string {
+		return `${1e7}-${1e3}-${4e3}-${8e3}-${1e11}`.replace(/[018]/g, (c) =>
+			(+c ^ (this.float(16) >> (+c / 4))).toString(16)
+		);
+	},
 };
