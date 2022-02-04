68c0b7bf1d8d712a7f914298181887684f479b4a
Ignatius Bagus <ignatius.mbs@gmail.com>
2022-02-04 22:05:38 +0700

    rename positive to natural

--- src/guards/index.ts
+++ src/guards/index.ts
@@ -5,6 +5,8 @@ type Primitives = string | number | bigint | boolean | symbol;
 
 export const exists = <T>(i: T | EmptyString | Nullish): i is T => !nullish(i) && i !== '';
 export const nullish = <T>(i: T | Primitives): i is T => i == null;
-export const positive = <T>(i: T | Exclude<Falsy, 0>): i is T =>
-	typeof i === 'number' ? i > 0 : exists(i);
 export const truthy = <T>(i: T | EmptyString | Nullish | Falsy): i is T => !!i;
+
+// number guards
+export const natural = <T>(i: T | Exclude<Falsy, 0>): i is T =>
+	typeof i === 'number' ? i > 0 : exists(i);
