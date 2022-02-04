93155e311f973e9f7a540b1536e2a11e96d80010
Ignatius Bagus <ignatius.mbs@gmail.com>
2022-02-04 22:15:38 +0700

    whole number guards

--- src/guards/index.ts
+++ src/guards/index.ts
@@ -8,5 +8,7 @@ export const nullish = <T>(i: T | Primitives): i is T => i == null;
 export const truthy = <T>(i: T | EmptyString | Nullish | Falsy): i is T => !!i;
 
 // number guards
-export const natural = <T>(i: T | Exclude<Falsy, 0>): i is T =>
-	typeof i === 'number' ? i > 0 : exists(i);
+/** @returns true if input exists or is a number greater than 0 */
+export const natural = <T>(i: T | Falsy): i is T => (typeof i === 'number' ? i > 0 : exists(i));
+/** @returns true if input exists or is a number greater than or equal to 0 */
+export const whole = <T>(i: T | Falsy): i is T => (typeof i === 'number' ? i >= 0 : exists(i));
