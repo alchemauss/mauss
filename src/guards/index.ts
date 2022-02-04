e970a03e6f4737b06af3c089a82f56c5a3678874
Ignatius Bagus <ignatius.mbs@gmail.com>
2022-02-04 21:30:53 +0700

    reworked exists

--- src/guards/index.ts
+++ src/guards/index.ts
@@ -1,2 +1,8 @@
-export const nullish = <T>(i: T | null | undefined): i is T => i == null;
-export const truthy = <T>(i: T | 0 | '' | false | null | undefined | typeof NaN): i is T => !!i;
+type EmptyString = '';
+type Nullish = null | undefined;
+type Falsy = 0 | false | EmptyString | Nullish;
+type Primitives = string | number | bigint | boolean | symbol;
+
+export const exists = <T>(i: T | EmptyString | Nullish): i is T => !nullish(i) && i !== '';
+export const nullish = <T>(i: T | Primitives): i is T => i == null;
+export const truthy = <T>(i: T | EmptyString | Nullish | Falsy): i is T => !!i;
