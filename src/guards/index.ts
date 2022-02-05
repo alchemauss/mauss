77480fb2fb87610d8f848dbc8e5b29bc31e894cc
Ignatius Bagus <ignatius.mbs@gmail.com>
2022-02-05 10:39:37 +0700

    simplify generic types

--- src/guards/index.ts
+++ src/guards/index.ts
@@ -1,14 +1,15 @@
 type EmptyString = '';
 type Nullish = null | undefined;
+type Empty = EmptyString | Nullish;
 type Falsy = 0 | false | EmptyString | Nullish;
 type Primitives = string | number | bigint | boolean | symbol;
 
 /** @returns true if input it not `nullish` or an empty string */
-export const exists = <T>(i: T | EmptyString | Nullish): i is T => !nullish(i) && i !== '';
+export const exists = <T>(i: T | Empty): i is T => !nullish(i) && i !== '';
 /** @returns true if input is `null` or `undefined` */
 export const nullish = <T>(i: T | Primitives): i is T => i == null;
 /** @returns true if input is truthy in general */
-export const truthy = <T>(i: T | EmptyString | Nullish | Falsy): i is T => !!i;
+export const truthy = <T>(i: T | Falsy): i is T => !!i;
 
 // number guards
 /** @returns true if input exists or is a number greater than 0 */
