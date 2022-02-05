86a169113405e893120ea6b5721d40e0cb269133
Ignatius Bagus <ignatius.mbs@gmail.com>
2022-02-05 10:42:19 +0700

    change number guards to exclude empty

--- src/guards/index.ts
+++ src/guards/index.ts
@@ -13,6 +13,6 @@ export const truthy = <T>(i: T | Falsy): i is T => !!i;
 
 // number guards
 /** @returns true if input exists or is a number greater than 0 */
-export const natural = <T>(i: T | Falsy): i is T => (typeof i === 'number' ? i > 0 : exists(i));
+export const natural = <T>(i: T | Empty): i is T => (typeof i === 'number' ? i > 0 : exists(i));
 /** @returns true if input exists or is a number greater than or equal to 0 */
-export const whole = <T>(i: T | Falsy): i is T => (typeof i === 'number' ? i >= 0 : exists(i));
+export const whole = <T>(i: T | Empty): i is T => (typeof i === 'number' ? i >= 0 : exists(i));
