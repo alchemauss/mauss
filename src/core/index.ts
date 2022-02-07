fa9c5180b66953297877a2c6f44d9954345db83f
Ignatius Bagus <ignatius.mbs@gmail.com>
2022-02-07 13:24:28 +0700

    inverse function

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,3 +1,13 @@
+type RestParameters = (...parameters: any) => any;
+/**
+ * inverse - reverses the order of provided arguments to fn parameters
+ * @param fn any function with one or more arguments
+ * @returns a curried function to take in the arguments
+ */
+export function inverse(fn: RestParameters): RestParameters {
+	return (...parameters) => fn(...parameters.reverse());
+}
+
 /**
  * unique - transform an array to a set and back to array
  * @param array items to be inspected
@@ -9,7 +19,7 @@ export function unique<T extends any[]>(array: T): T[] {
 
 /**
  * regexp - implementation of global RegExp constructor with escaped pattern
- * @param exp pattern in the form of string literal
+ * @param pattern pattern in the form of string literal
  * @param flags unique set of characters from `d|g|i|m|s|u|y`
  * @returns dynamically constructed RegExp object
  */
