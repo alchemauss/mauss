22d78491d9618135b6f2a0bbb0a9166cf844570a
Ignatius Bagus <ignatius.mbs@gmail.com>
2022-02-15 21:43:19 +0700

    add query string decoder

--- src/web/index.ts
+++ src/web/index.ts
@@ -1,4 +1,5 @@
-import type { Primitives } from '../typings';
+import type { Primitives, PotArray } from '../typings';
+import { tryNumber } from '../utils';
 
 export * as cookies from './cookies';
 
@@ -22,4 +23,31 @@ export function qse(bound: Record<string, Primitives>): string {
 	}
 
 	return final;
+}
+
+/**
+ * qsd - query string decoder
+ * @param qs query string of a URL with or without the leading `?`
+ * @returns mapped object of decoded query string
+ */
+export function qsd(qs: string) {
+	if (qs[0] === '?') qs = qs.slice(1).trim();
+	if (!qs) return {};
+
+	const dec = (s: string) => {
+		if (!s.trim()) return '';
+		s = decodeURIComponent(s);
+		return ['true', 'false'].includes(s) ? s[0] === 't' : tryNumber(s);
+	};
+
+	const dqs: Record<string, PotArray<Primitives>> = {};
+	const qar = qs.split('&');
+	for (let i = 0; i < qar.length; i++) {
+		const [k, v] = qar[i].split('=');
+		const item = dqs[k]; // satisfy TS
+		if (!dqs[k] && (dqs[k] = dec(v))) continue;
+		if (!Array.isArray(item)) dqs[k] = [item];
+		(dqs[k] as Primitives[]).push(dec(v));
+	}
+	return dqs;
 }
