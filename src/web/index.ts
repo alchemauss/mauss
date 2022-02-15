0161355e618c5abcec54d596a3d1234b5524fb65
Ignatius Bagus <ignatius.mbs@gmail.com>
2022-02-15 18:25:14 +0700

    add second parameter to receive prefix

--- src/web/index.ts
+++ src/web/index.ts
@@ -3,11 +3,12 @@ import * as cookies from './cookies';
 export { cookies };
 
 /**
- * qpm - Query string Pathname Maker
+ * qpm - query string pathname maker
  * @param bound object with key-value pair to be updated in the URL
+ * @param prefix string to prepend on the final output
  * @returns final pathname with query string
  */
-export function qpm(bound: Record<string, string | number | boolean>): string {
+export function qpm(bound: Record<string, string | number | boolean>, prefix = ''): string {
 	if (typeof window === 'undefined') return '';
 	const kvs = Object.entries(bound).reduce((a, [k, v]) => {
 		if (typeof v === 'string') v = v.trim();
