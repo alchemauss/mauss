79e9eca7b727d78e8e4830f3fe026e13b8976b09
Ignatius Bagus <ignatius.mbs@gmail.com>
2022-02-18 23:05:58 +0700

    type safe TryValidator

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -13,5 +13,18 @@ export function capitalize(text: string, { cap, normalize }: CapitalizeOptions =
 	return text.replace(/(?:^|\s)\S/g, (a) => a.toUpperCase());
 }
 
-export const tryNumber = <T>(text: T, fallback = text): T | number =>
-	Number.isNaN(+text) ? fallback : +text;
+type Numeric = '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9';
+type TryValidator<Text extends string> = Text extends `${infer Character}${infer Rest}`
+	? [Character, TryValidator<Rest>] extends [Numeric, infer Validated]
+		? [Validated] extends ['']
+			? number
+			: Validated extends string
+			? Text
+			: number
+		: Text
+	: Text;
+
+export function tryNumber<T extends string | number>(text: T, fallback = text) {
+	type TryReturned = T extends string ? TryValidator<T> : number;
+	return (Number.isNaN(+text) ? fallback : +text) as TryReturned;
+}
