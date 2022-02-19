c7361a6a0fbda3bb83c116ab216f0e93b96877af
Ignatius Bagus <ignatius.mbs@gmail.com>
2022-02-19 18:32:16 +0700

    update tolerance

--- test/performance/utils.test.ts
+++ test/performance/utils.test.ts
@@ -3,6 +3,8 @@ import * as assert from 'uvu/assert';
 import * as utils from '../../src/utils';
 import snapshot from '../benchmark';
 
+const TOLERANCE = 5;
+
 const mapCap = (t: string) => t[0].toUpperCase() + t.slice(1);
 const native = (text: string) => !!text.split(' ').map(mapCap).join(' ');
 const mauss = (text: string) => !!utils.capitalize(text);
@@ -11,8 +13,8 @@ test('comparison with Array check', () => {
 	const [m, n] = [snapshot(mauss), snapshot(native)];
 	const ms = m.time - n.time;
 	const kb = m.memory - n.memory;
-	assert.ok(ms <= 1, `native is ${ms} faster`);
-	assert.ok(kb <= 1, `native is ${kb} lighter`);
+	assert.ok(ms <= TOLERANCE, `native is ${ms} faster`);
+	assert.ok(kb <= TOLERANCE, `native is ${kb} lighter`);
 });
 
 test.run();
