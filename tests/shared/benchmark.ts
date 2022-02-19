5364f3c7be66fc290cfc67266aacb6991a1bb5c6
Ignatius Bagus <ignatius.mbs@gmail.com>
2022-02-19 13:15:20 +0700

    merge runtim and memory into snapshot

--- tests/shared/benchmark.ts
+++ tests/shared/benchmark.ts
@@ -1,22 +1,21 @@
 import { readFileSync } from 'fs';
 import { performance } from 'perf_hooks';
 
-const file = readFileSync(`${__dirname}/data.json`, 'utf-8');
-const data = JSON.parse(file);
+const file = `${process.cwd()}/tests/shared/data.json`;
+const data = JSON.parse(readFileSync(file, 'utf-8'));
 
-export function runtime(fn: (input: string) => boolean) {
-	const time = performance.now();
-	for (let i = 0; i < data.length; i++) {
-		if (fn(data[i]['body'])) continue;
-	}
-	return performance.now() - time;
-}
-
-export function memory(fn: (input: string) => boolean) {
+export default function snapshot(fn: (input: string) => boolean) {
 	const initial = process.memoryUsage().heapUsed / 1024 / 1024;
+	const time = performance.now();
+
 	for (let i = 0; i < data.length; i++) {
 		if (fn(data[i]['body'])) continue;
 	}
+
 	const final = process.memoryUsage().heapUsed / 1024 / 1024;
-	return { initial, final, diff: final - initial };
+
+	return {
+		memory: final - initial,
+		time: performance.now() - time,
+	};
 }
