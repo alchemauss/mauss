80cf6d1e2c7564901bb8e6a085445dca44f19e25
Ignatius Bagus <ignatius.mbs@gmail.com>
2022-02-20 14:07:33 +0700

    simplest working one

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,4 +1,4 @@
-import type { AnyFunction, Last, Reverse, UnaryFunction } from '../typings';
+import type { AnyFunction, Reverse } from '../typings';
 
 /**
  * inverse - reverses the order of provided arguments to fn parameters
@@ -7,19 +7,6 @@ import type { AnyFunction, Last, Reverse, UnaryFunction } from '../typings';
  */
 export function inverse<T extends AnyFunction>(fn: T): AnyFunction<Reverse<Parameters<T>>> {
 	return (...parameters) => fn(...parameters.reverse());
-}
-
-// TODO: validate the next parameter receive the same return type as the previous
-export function pipe<Functions extends UnaryFunction[]>(...functions: Functions) {
-	type InitialType = Parameters<Functions[0]>[0];
-	type FinalType = ReturnType<Last<Functions, any>>;
-	return (arg: InitialType): FinalType => {
-		let pipeline = arg;
-		for (let i = 0; i < functions.length; i++) {
-			pipeline = functions[i](pipeline);
-		}
-		return pipeline;
-	};
 }
 
 /**
