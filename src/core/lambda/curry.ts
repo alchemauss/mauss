2564cdab7e7ec55741adcaa27784321952f0ea84
Ignatius Bagus <ignatius.mbs@gmail.com>
2022-02-20 22:18:35 +0700

    remove `any[]`

--- src/core/lambda/curry.ts
+++ src/core/lambda/curry.ts
@@ -5,6 +5,6 @@ export default function curry<F extends AnyFunction>(fn: F, expected = fn.length
 	// TODO: only allow a set amount of argument combinations
 	return <Arguments extends Parameters<F>>(...args: Arguments) => {
 		if (args.length >= expected) return fn(...args);
-		return curry((...next: any[]) => fn(...[...args, ...next]), expected - args.length);
+		return curry((...next) => fn(...[...args, ...next]), expected - args.length);
 	};
 }
