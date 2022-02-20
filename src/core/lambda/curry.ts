827c389dcd4e7cf575114f101702a04774e2960b
Ignatius Bagus <ignatius.mbs@gmail.com>
2022-02-20 21:56:48 +0700

    add some todo comments

--- src/core/lambda/curry.ts
+++ src/core/lambda/curry.ts
@@ -1,6 +1,8 @@
 import type { AnyFunction } from '../../typings';
 
+// TODO: specify return type with Curried<F>
 export default function curry<F extends AnyFunction>(fn: F, expected = fn.length) {
+	// TODO: only allow a set amount of argument combinations
 	return <Arguments extends Parameters<F>>(...args: Arguments) => {
 		if (args.length >= expected) return fn(...args);
 		return curry((...next: any[]) => fn(...[...args, ...next]), expected - args.length);
