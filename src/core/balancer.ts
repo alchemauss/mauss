8703915e280a46e185c9e693d13d3b9636ab0dea
Ignatius Bagus <ignatius.mbs@gmail.com>
2022-02-22 17:14:47 +0700

    strongly type curried parameters

--- src/core/balancer.ts
+++ src/core/balancer.ts
@@ -2,7 +2,7 @@ import type { AnyFunction } from '../typings';
 
 export function debounce<F extends AnyFunction>(fn: F, time = 300) {
 	let timeout: NodeJS.Timeout;
-	return (...args: any[]) => {
+	return <A extends Parameters<F>>(...args: A) => {
 		if (timeout) clearTimeout(timeout);
 		timeout = setTimeout(() => fn(...args), time);
 	};
@@ -10,7 +10,7 @@ export function debounce<F extends AnyFunction>(fn: F, time = 300) {
 
 export function throttle<F extends AnyFunction>(fn: F, time = 300) {
 	let wait = false;
-	return (...args: any[]) => {
+	return <A extends Parameters<F>>(...args: A) => {
 		if (wait) return;
 		fn(...args), (wait = true);
 		setTimeout(() => (wait = false), time);
