cc30df2c93f9ca0d4c20b3d8110b9753616ce71b
Ignatius Bagus <ignatius.mbs@gmail.com>
2022-02-26 17:30:42 +0700

    add tryNumber suite

--- src/utils/utils.spec.ts
+++ src/utils/utils.spec.ts
@@ -2,13 +2,32 @@ import { suite } from 'uvu';
 import assert from 'uvu/assert';
 import * as utils from '.';
 
-const basics = [suite('capitalize')];
+const basics = [suite('capitalize'), suite('tryNumber')];
+
+// --- capitalize ---
 
 basics[0]('change one letter for one word', () => {
 	assert.equal(utils.capitalize('hello'), 'Hello');
 });
 basics[0]('change two letter for two words', () => {
 	assert.equal(utils.capitalize('hello world'), 'Hello World');
+});
+
+// --- tryNumber ---
+
+basics[1]('convert numbers properly', () => {
+	assert.equal(utils.tryNumber('0'), 0);
+	assert.equal(utils.tryNumber('1'), 1);
+	assert.equal(utils.tryNumber('-1'), -1);
+	assert.equal(utils.tryNumber('1e3'), 1e3);
+});
+
+basics[1]('convert strings as-is', () => {
+	assert.equal(utils.tryNumber('a'), 'a');
+	assert.equal(utils.tryNumber('a1a'), 'a1a');
+	assert.equal(utils.tryNumber('1a1'), '1a1');
+	assert.equal(utils.tryNumber('-1a'), '-1a');
+	assert.equal(utils.tryNumber('-1e'), '-1e');
 });
 
 basics.forEach((v) => v.run());
