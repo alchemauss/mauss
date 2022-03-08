ca3e09003ab20248cf6c0efe5416183202fcc46c
Ignatius Bagus <ignatius.mbs@gmail.com>
2022-03-08 16:19:13 +0700

    type test for throttle

--- src/core/balancer.test.ts
+++ src/core/balancer.test.ts
@@ -11,9 +11,22 @@ expect<UnaryFunction<string>>(debounce(str));
 expect<UnaryFunction<number>>(debounce(num));
 expect<UnaryFunction<{ name: string }>>(debounce(name));
 
+expect<UnaryFunction<string>>(throttle(str));
+expect<UnaryFunction<number>>(throttle(num));
+expect<UnaryFunction<{ name: string }>>(throttle(name));
+
+// ---- errors ----
+
 // @ts-expect-error
 expect<UnaryFunction<number>>(debounce(str));
 // @ts-expect-error
 expect<UnaryFunction<string>>(debounce(num));
 // @ts-expect-error
 expect<UnaryFunction<{ name: boolean }>>(debounce(name));
+
+// @ts-expect-error
+expect<UnaryFunction<number>>(throttle(str));
+// @ts-expect-error
+expect<UnaryFunction<string>>(throttle(num));
+// @ts-expect-error
+expect<UnaryFunction<{ name: boolean }>>(throttle(name));
