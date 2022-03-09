3658fe289819e0bc5b1fe6bc9395a87470db7f0d
Ignatius Bagus <ignatius.mbs@gmail.com>
2022-03-09 22:24:33 +0700

    add type comparators

--- src/typings.ts
+++ src/typings.ts
@@ -16,6 +16,13 @@ export type TypeTable = TypePrimitive | 'undefined' | 'object' | 'function';
 /** Expand T to also be an array */
 export type AlsoArray<T> = T | T[];
 
+/* <-- Type Comparators: Prefixed with `When` --> */
+
+/** When T is type of `any`, returns `Y`, else `N` */
+export type WhenAny<T, Y = true, N = false> = 0 extends 1 & T ? Y : N;
+/** When T is type of `unknown`, returns `Y`, else `N` */
+export type WhenUnknown<T, Y = true, N = false> = unknown extends T ? Y : N;
+
 /* <-- Compact Type Helpers --> */
 
 /** Generic for making any arbitrary function */
