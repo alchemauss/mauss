75dd5f8760d12e0abdb9512ad6dd25c5083dee3d
Ignatius Bagus <ignatius.mbs@gmail.com>
2022-06-24 12:08:36 +0700

    fix undefined compare method

--- src/core/inspector.spec.ts
+++ src/core/inspector.spec.ts
@@ -5,7 +5,20 @@ import { compare, comparator } from './inspector';
 const basics = {
 	comparator: suite('comparator'),
 
+	wildcard: suite('compare:wildcard'),
+
+	undefined: suite('compare:undefined'),
+	boolean: suite('compare:boolean'),
 	number: suite('compare:number'),
+	bigint: suite('compare:bigint'),
+	symbol: suite('compare:symbol'),
+	string: suite('compare:string'),
+	object: suite('compare:object'),
+
+	date: suite('compare:date'),
+	time: suite('compare:time'),
+
+	key: suite('compare:key'),
 };
 
 // ---- standard ----
@@ -21,6 +34,12 @@ basics.comparator('comparator', () => {
 	]);
 });
 
+basics.undefined('sort undefined values with null values above', () => {
+	assert.equal(
+		[undefined, 3, 0, null, 1, -1, undefined, -2, undefined, null].sort(compare.undefined),
+		[3, 0, 1, -1, -2, null, null, undefined, undefined, undefined]
+	);
+});
 basics.number('sort number in descending order', () => {
 	assert.equal(
 		[5, 3, 9, 6, 0, 2, 1, -1, 4, -2].sort(compare.number),
