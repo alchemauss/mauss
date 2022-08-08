39dcc7638a5ccb1bf6a426f06ca1d8b0ea95717f
Ignatius Bagus <ignatius.mbs@gmail.com>
2022-08-08 15:16:30 +0700

    failing test

--- src/utils/random.spec.ts
+++ src/utils/random.spec.ts
@@ -1,3 +1,4 @@
+import { webcrypto as crypto } from 'node:crypto';
 import { suite } from 'uvu';
 import * as assert from 'uvu/assert';
 import * as random from './random.js';
@@ -11,6 +12,7 @@ const basics = {
 	val: suite('random:val'),
 	// hex: suite('random:hex'),
 	// ipv4: suite('random:ipv4'),
+	uuid: suite('random:uuid'),
 };
 
 // ---- standard ----
@@ -20,28 +22,43 @@ basics.float('generate random float', () => {
 	assert.type(number, 'number');
 	assert.ok(number >= 0 && number <= 1);
 });
+
 basics.int('generate random integer', () => {
 	const number = random.int();
 	assert.type(number, 'number');
 	assert.ok(number === 0 || number === 1);
 });
+
 basics.bool('generate random bool', () => {
 	assert.type(random.bool(), 'boolean');
 });
+
 basics.array('generate array with random values', () => {
 	const array = random.array(5, 3);
 	assert.type(array, 'object');
 	assert.equal(array.length, 5);
 });
+
 basics.key('get random key from object', () => {
 	const key = random.key({ foo: 0, bar: 1 });
 	assert.type(key, 'string');
 	assert.ok(key === 'foo' || key === 'bar');
 });
+
 basics.val('get random value from object', () => {
 	const val = random.val({ foo: 0, bar: 1 });
 	assert.type(val, 'number');
 	assert.ok(val === 0 || val === 1);
+});
+
+basics.uuid('generate random uuid', () => {
+	const floating = random.uuid();
+	assert.equal(floating.length, 36);
+	assert.equal(floating.split('-').length, 5);
+
+	const secure = random.uuid(crypto);
+	assert.equal(secure.length, 36);
+	assert.equal(secure.split('-').length, 5);
 });
 
 Object.values(basics).forEach((v) => v.run());
