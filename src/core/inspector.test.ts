806ac4a206613eec55261beaed1cc089eabd354b
Ignatius Bagus <ignatius.mbs@gmail.com>
2022-08-29 16:56:04 +0700

    failing type test

--- src/core/inspector.test.ts
+++ src/core/inspector.test.ts
@@ -7,6 +7,7 @@ let unknown: Record<any, unknown> = {};
 compare.key('name')(generic, generic);
 compare.key('name')({ name: 'abc' }, { name: 'def', foo: maybe ? 1 : undefined });
 compare.key('name', compare.string)({ name: 'abc' }, { name: 'def' });
+compare.key('name.first', compare.string)({ name: { first: 'abc' } }, { name: { first: 'def' } });
 
 // ---- errors ----
 
@@ -16,5 +17,7 @@ compare.key('name')(unknown, unknown);
 compare.key('name')({}, { name: 'abc' });
 // @ts-expect-error
 compare.key('name')({ name: 'abc' }, {});
+// @ts-expect-error
+compare.key('name.last')({ name: { first: 'abc' } }, { name: { first: 'def' } });
 // @ts-expect-error
 compare.key('nope')({ name: 'abc' }, { name: 'def' });
