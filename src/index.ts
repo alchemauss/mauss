1b645002ed99909716ba1904d90944b4a0ea7876
Ignatius Bagus <ignatius.mbs@gmail.com>
2022-08-29 17:17:41 +0700

    cast export star with name

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export * from './core/index.js';
 export * from './core/balancer.js';
-export * from './core/inspector.js';
+export * as compare from './core/inspector.js';
 
 export { default as curry } from './core/lambda/curry.js';
 export { default as pipe } from './core/lambda/pipe.js';
