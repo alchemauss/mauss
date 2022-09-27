f360e29402033f5dd923796b7dd4a38dbb690e36
Ignatius Bagus <ignatius.mbs@gmail.com>
2022-09-27 14:08:44 +0700

    more type comparators

--- src/typings/comparators.ts
+++ src/typings/comparators.ts
@@ -1,6 +1,16 @@
 /* <-- Type Comparators: Prefixed with `When` --> */
 
+/** When L extends R, returns `Y`, else `N` */
+export type When<L, R, Y = true, N = never> = L extends R ? Y : N;
+
 /** When T is type of `any`, returns `Y`, else `N` */
-export type WhenAny<T, Y = true, N = false> = 0 extends 1 & T ? Y : N;
+export type WhenAny<T, Y = true, N = false> = When<0, 1 & T, Y, N>;
+
+/** When T is type of `never`, returns `Y`, else `N` */
+export type WhenNever<T, Y = true, N = false> = When<[T], [never], Y, N>;
+
+/** When T is type of `object`, returns `Y`, else `N` */
+export type WhenObject<T, Y = true, N = false> = When<T, object, Y, N>;
+
 /** When T is type of `unknown`, returns `Y`, else `N` */
-export type WhenUnknown<T, Y = true, N = false> = unknown extends T ? Y : N;
+export type WhenUnknown<T, Y = true, N = false> = When<unknown, T, Y, N>;
