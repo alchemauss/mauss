a5d325130ceeafd481d299b53c80dd6b59f31567
Ignatius Bagus <ignatius.mbs@gmail.com>
2022-10-05 14:52:54 +0700

    cover more cases

--- src/std/sys/path.spec.ts
+++ src/std/sys/path.spec.ts
@@ -1,10 +1,17 @@
-import { test } from 'uvu';
+import { suite } from 'uvu';
 import * as assert from 'uvu/assert';
 import * as path from './path.js';
 
-test('joins paths correctly', () => {
+const basics = {
+	join: suite('path:join'),
+};
+
+basics.join('joins paths base cases', () => {
 	assert.equal(path.join(''), '');
 	assert.equal(path.join('/'), '/');
+});
+
+basics.join('joins URL-like paths correctly', () => {
 	assert.equal(path.join('/', 'root', ':id'), '/root/:id');
 	assert.equal(path.join('/root', ':id'), '/root/:id');
 	assert.equal(path.join('relative', ':id'), 'relative/:id');
@@ -12,4 +19,11 @@ test('joins paths correctly', () => {
 	assert.equal(path.join('', '', '', 'foo', '', ':id'), 'foo/:id');
 });
 
-test.run();
+basics.join('joins file-like paths correctly', () => {
+	assert.equal(path.join('/root', './user'), '/root/user');
+	assert.equal(path.join('/root/usr/', '..'), '/root');
+	assert.equal(path.join('/mnt/srv', './usr/../backup'), '/mnt/srv/backup');
+	assert.equal(path.join('../usr', './backup'), '../usr/backup');
+});
+
+Object.values(basics).forEach((v) => v.run());
--- src/std/sys/path.ts
+++ src/std/sys/path.ts
@@ -1,9 +1,16 @@
 export function join(...paths: string[]): string {
-	const index = paths[0].trim();
-	let final = index.replace(/[/]*$/g, '');
+	const index = paths[0].replace(/\\/g, '/').trim();
+	const parts = index.replace(/[/]*$/g, '').split('/');
+	if (parts[0] === '') parts.shift();
+
 	for (let i = 1; i < paths.length; i += 1) {
-		const part = paths[i].trim().replace(/(^[/]*|[/]*$)/g, '');
-		if (part) final = final ? `${final}/${part}` : part;
+		const part = paths[i].replace(/\\/g, '/').trim();
+		for (const slice of part.split('/')) {
+			if (slice === '.') continue;
+			if (slice === '..') parts.pop();
+			else if (slice) parts.push(slice);
+		}
 	}
-	return (index === '/' ? '/' : '') + final;
+
+	return (index[0] === '/' ? '/' : '') + parts.join('/');
 }
