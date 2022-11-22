fe98e109708243b68e4e9a281351edbdf09a5d52
Ignatius Bagus <ignatius.mbs@gmail.com>
2022-11-22 15:57:55 +0700

    feat: expose clipboard essentials

--- src/web/index.ts
+++ src/web/index.ts
@@ -1,3 +1,4 @@
 export * as cookies from './cookies.js';
+export * as clipboard from './navigator/clipboard.js';
 export { default as qsd } from './query/decoder.js';
 export { default as qse } from './query/encoder.js';
