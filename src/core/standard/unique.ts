0e8e208a2798fbaa96dfe20a20de68c34769faad
Ignatius Bagus <ignatius.mbs@gmail.com>
2022-12-02 18:13:46 +0700

    use map object

--- src/core/standard/unique.ts
+++ src/core/standard/unique.ts
@@ -6,7 +6,6 @@ import type { Paths } from '../../typings/prototypes.js';
  * @param array items to be inspected
  * @returns duplicate-free version of the array input
  */
-
 export default function unique<
 	Inferred extends Record<IndexSignature, any>,
 	Identifier extends Paths<Inferred>
@@ -15,11 +14,11 @@ export default function unique<T extends Primitives>(array: readonly T[]): T[];
 export default function unique<T, I>(array: readonly T[], key?: string & I): T[] {
 	if (!key || typeof array[0] !== 'object') return [...new Set(array)];
 
-	const filtered = Object.create(null);
 	const trail = key.split('.');
+	const filtered = new Map<string, any>();
 	for (const item of array as Record<IndexSignature, any>[]) {
 		const value: any = trail.reduce((r, p) => (r || {})[p], item);
-		if (value && !(value in filtered)) filtered[value] = item;
+		if (value && !filtered.has(value)) filtered.set(value, item);
 	}
-	return Object.values(filtered);
+	return [...filtered.values()];
 }
