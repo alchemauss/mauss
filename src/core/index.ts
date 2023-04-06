a64a48b3d10dcf25131ca93e4deb2c41108ab9ea
Ignatius Bagus <ignatius.mbs@gmail.com>
2023-04-06 17:13:23 +0700

    rearrange

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,7 +1,6 @@
+export * from './lambda/index.js';
 export * from './processor/index.js';
 export * from './standard/index.js';
 
 export * as compare from './compare/index.js';
-export { default as curry } from './lambda/curry.js';
-export { default as pipe } from './lambda/pipe.js';
 export { default as unique } from './standard/unique.js';
---
+++ src/core/lambda/index.ts
@@ -0,0 +1,2 @@
+export { default as curry } from './curry.js';
+export { default as pipe } from './pipe.js';
