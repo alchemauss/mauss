0c5e039465a81717f394e33e087587d635ac78e2
Ignatius Bagus <ignatius.mbs@gmail.com>
2023-05-02 16:36:35 +0700

    adapted from marqua

---
+++ src/std/yaml/index.spec.ts
@@ -0,0 +1,141 @@
+import { test } from 'uvu';
+import * as assert from 'uvu/assert';
+import { read } from './index.js';
+
+test('construct simple index', () => {
+	const index = read(
+		`
+title: Simple Index
+tags: [x, y, z]
+		`.trim()
+	);
+
+	assert.equal(index, {
+		title: 'Simple Index',
+		tags: ['x', 'y', 'z'],
+	});
+});
+test('construct marqua rules', () => {
+	const index = read(
+		`
+title: Marqua Rules
+date:published: 2023-02-01
+a:b:x: 0
+a:b:y: 1
+a:b:z: 2
+		`.trim()
+	);
+
+	assert.equal(index, {
+		title: 'Marqua Rules',
+		date: { published: '2023-02-01' },
+		a: { b: { x: '0', y: '1', z: '2' } },
+	});
+});
+test('convert boolean values', () => {
+	const index = read(
+		`
+title: Casting Boolean
+draft: false
+hex: ["x", true, 0, false]
+		`.trim()
+	);
+
+	assert.equal(index, {
+		title: 'Casting Boolean',
+		draft: false,
+		hex: ['x', true, '0', false],
+	});
+});
+test('construct literal block', () => {
+	const index = read(
+		`
+title: Literal Block
+data: |
+	Hello World
+	Lorem Ipsum
+		`.trim()
+	);
+
+	assert.equal(index, {
+		title: 'Literal Block',
+		data: 'Hello World\nLorem Ipsum',
+	});
+});
+test('construct sequences', () => {
+	const index = read(
+		`
+title: List Sequence
+hex:
+	- 'x'
+	- true
+	- 0
+		`.trim()
+	);
+
+	assert.equal(index, {
+		title: 'List Sequence',
+		hex: ['x', true, '0'],
+	});
+});
+test('construct indents', () => {
+	const index = read(
+		`
+title: Indented Objects
+jobs:
+	test:
+		with: node
+	sync:
+		with: pnpm
+		`.trim()
+	);
+
+	assert.equal(index, {
+		title: 'Indented Objects',
+		jobs: {
+			test: { with: 'node' },
+			sync: { with: 'pnpm' },
+		},
+	});
+});
+test('construct indented sequences', () => {
+	const index = read(
+		`
+title: Indented Objects and Arrays
+jobs:
+	test:
+		- with: node
+			os: windows
+	sync:
+		- with: pnpm
+			os: linux
+			env:
+				TOKEN: 123
+		`.trim()
+	);
+
+	assert.equal(index, {
+		title: 'Indented Objects and Arrays',
+		jobs: {
+			test: [{ with: 'node', os: 'windows' }],
+			sync: [{ with: 'pnpm', os: 'linux', env: { TOKEN: '123' } }],
+		},
+	});
+});
+test('handle edge cases', () => {
+	const index = read(
+		`
+title: Edge Cases
+name: "Hello: World"
+link: https://github.com
+		`.trim()
+	);
+
+	assert.equal(index, {
+		title: 'Edge Cases',
+		name: 'Hello: World',
+		link: 'https://github.com',
+	});
+});
+
+test.run();
---
+++ src/std/yaml/index.ts
@@ -0,0 +1,63 @@
+type Primitives = null | boolean | string;
+type FrontMatter = { [key: string]: Primitives | Primitives[] | FrontMatter | FrontMatter[] };
+export function read(raw: string, memo: Record<string, any> = {}): FrontMatter[string] {
+	const indent = indentation(raw);
+	if (!/[:\-\[\]|#]/gm.test(raw)) {
+		return indent > 1 ? dedent(raw) : coerce(raw.trim());
+	}
+	if (indent <= 1) raw = raw.trimStart();
+	if (/^(".*"|'.*')$/.test(raw.trim())) {
+		return raw.trim().slice(1, -1);
+	}
+
+	const PATTERN = /(^[^:\s]+):(?!\/)\n?([\s\S]*?(?=^\S)|[\s\S]*$)/gm;
+	let match: null | RegExpExecArray;
+	while ((match = PATTERN.exec(raw))) {
+		const [, key, value] = match;
+		const i = indentation(value);
+		const data = read(i ? dedent(value) : value, memo[key]);
+		if (Array.isArray(data) || typeof data !== 'object') memo[key] = data;
+		else memo[key] = { ...memo[key], ...data };
+	}
+
+	if (Object.keys(memo).length) return memo;
+
+	const cleaned = raw.replace(/#.*$/gm, '').trim();
+	switch (cleaned[0]) {
+		case '-': {
+			const sequence = cleaned.split('-').filter((v) => v);
+			type Possibly = Primitives & FrontMatter; // what the ...?
+			return sequence.map((v) => read(dedent(v)) as Possibly);
+		}
+		case '[': {
+			const pruned = cleaned.slice(1, -1);
+			return pruned.split(',').map(coerce);
+		}
+		case '|': {
+			return dedent(cleaned.slice(1).replace('\n', ''));
+		}
+		default: {
+			return coerce(cleaned.trim());
+		}
+	}
+}
+
+// ---- internal functions ----
+
+function coerce(u: string) {
+	const v = u.trim(); // argument can be passed as-is
+	const map = { true: true, false: false, null: null };
+	if (v in map) return map[v as keyof typeof map];
+	// if (!Number.isNaN(Number(v))) return Number(v);
+	return /^(".*"|'.*')$/.test(v) ? v.slice(1, -1) : v;
+}
+
+function dedent(input: string) {
+	const indent = indentation(input);
+	const lines = input.split(/\r?\n/);
+	return lines.map((l) => l.slice(indent)).join('\n');
+}
+
+function indentation(line: string) {
+	return (/^\s*/.exec(line) || [''])[0].length;
+}
