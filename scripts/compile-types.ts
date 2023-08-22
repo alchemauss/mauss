0282184ad0d8e544bc4b90ec9f924e5b86201384
Ignatius Bagus <ignatius.mbs@gmail.com>
2023-08-22 13:46:20 +0700

    debug script

--- scripts/compile-types.ts
+++ scripts/compile-types.ts
@@ -3,8 +3,19 @@ import { exports } from '../package.json';
 
 await createBundle({
 	output: 'index.d.ts',
-	modules: Object.entries(exports).reduce((acc, [key, entry]) => {
-		if (key.slice(2).includes('.')) return acc; // skip non-modules
-		return { ...acc, ['mauss' + key.slice(1)]: entry };
-	}, {}),
+	project: 'src/tsconfig.json',
+	// modules: Object.entries(exports).reduce((acc, [key, entry]) => {
+	// 	if (key.slice(2).includes('.')) return acc; // skip non-modules
+	// 	return { ...acc, ['mauss' + key.slice(1)]: entry };
+	// }, {}),
+	modules: {
+		// "mauss": "./src/core/index.js",
+		'mauss/api': './src/api/index.js',
+		'mauss/bits': './src/bits/index.js',
+		'mauss/guards': './src/guards/index.js',
+		'mauss/math': './src/math/index.js',
+		// 'mauss/std': './src/std/index.js',
+		// 'mauss/web': './src/web/index.js',
+		'mauss/typings': './src/typings/index.d.ts',
+	},
 });
