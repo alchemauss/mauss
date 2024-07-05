e479404e56a2215a8e8609b6f475fd02ee428ef9
Ignatius Bagus <ignatius.mbs@gmail.com>
2024-07-05 12:22:46 +0700

    fix: specify `"types"` in `"exports"` (#250)

--- workspace/mauss/scripts/compile-types.ts
+++ workspace/mauss/scripts/compile-types.ts
@@ -4,8 +4,9 @@ import { exports } from '../package.json';
 await createBundle({
 	output: 'index.d.ts',
 	project: 'src/tsconfig.json',
-	modules: Object.entries(exports).reduce((acc, [key, entry]) => {
-		if (key.slice(2).includes('.')) return acc; // skip non-modules
+	modules: Object.keys(exports).reduce((acc, key) => {
+		if (key.slice(2).includes('.')) return acc;
+		const entry = `./src/${key.slice(2) || 'core'}/index.js`;
 		return { ...acc, ['mauss' + key.slice(1)]: entry };
 	}, {}),
 });
