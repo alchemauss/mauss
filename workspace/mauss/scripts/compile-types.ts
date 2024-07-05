87aa413c32cbd2e4a90acbf0c1bec4c27f4e4b57
Ignatius Bagus <ignatius.mbs@gmail.com>
2024-07-05 12:21:29 +0700

    simplify

--- workspace/mauss/scripts/compile-types.ts
+++ workspace/mauss/scripts/compile-types.ts
@@ -4,9 +4,9 @@ import { exports } from '../package.json';
 await createBundle({
 	output: 'index.d.ts',
 	project: 'src/tsconfig.json',
-	modules: Object.entries(exports).reduce((acc, [key, exp]) => {
+	modules: Object.keys(exports).reduce((acc, key) => {
 		if (key.slice(2).includes('.')) return acc;
-		const entry = typeof exp === 'object' ? exp.default : exp;
+		const entry = `./src/${key.slice(2) || 'core'}/index.js`;
 		return { ...acc, ['mauss' + key.slice(1)]: entry };
 	}, {}),
 });
