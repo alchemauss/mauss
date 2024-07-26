f8e3c809bd342db9e37ee873a45036ac412d6a52
Ignatius Bagus <ignatius.mbs@gmail.com>
2024-07-26 15:57:10 +0700

    remove default keyword

--- workspace/mauss/src/core/index.ts
+++ workspace/mauss/src/core/index.ts
@@ -2,7 +2,6 @@ export { curry, pipe } from './lambda/index.js';
 
 export { debounce, immediate, throttle } from './processor/index.js';
 
-export { capitalize, identical, inverse, regexp, scope } from './standard/index.js';
-export { default as unique } from './standard/unique.js';
+export { capitalize, identical, inverse, regexp, scope, unique } from './standard/index.js';
 
-export { default as tsf } from './tsf/index.js';
+export { tsf } from './tsf/index.js';
