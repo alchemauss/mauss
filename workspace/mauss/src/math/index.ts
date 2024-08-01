b80efda2e01fe003e17eac1a5e7a99fbd28227d5
Ignatius Bagus <ignatius.mbs@gmail.com>
2024-08-01 20:51:04 +0700

    feat: add `modulo` function

--- workspace/mauss/src/math/index.ts
+++ workspace/mauss/src/math/index.ts
@@ -3,3 +3,19 @@ export { permutation } from './set/index.js';
 export function clamp(min: number, max: number) {
 	return (value: number) => Math.max(min, Math.min(value, max));
 }
+
+/**
+ * The `%` is a remainder operator, this function computes the modulo operation and ensures a non-negative number. The result is always in the range `[0, n)`.
+ *
+ * @example
+ * ```javascript
+ * // returns 1
+ * modulo(5, 2);
+ *
+ * // returns 1
+ * modulo(-3, 2);
+ * ```
+ */
+export function modulo(a: number, n: number): number {
+	return ((a % n) + n) % n;
+}
