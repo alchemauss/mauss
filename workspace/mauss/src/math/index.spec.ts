2cc11332faaa671d444b0806af0008273cf93b4b
Ignatius Bagus <ignatius.mbs@gmail.com>
2024-08-09 14:46:10 +0700

    add test

--- workspace/mauss/src/math/index.spec.ts
+++ workspace/mauss/src/math/index.spec.ts
@@ -4,6 +4,7 @@ import * as math from './index.js';
 
 const suites = {
 	'average/': suite('math/average'),
+	'modulo/': suite('math/modulo'),
 };
 
 suites['average/']('average base cases', () => {
@@ -12,6 +13,21 @@ suites['average/']('average base cases', () => {
 suites['average/']('average numbers', () => {
 	assert.equal(math.average([1, 2, 3, 4, 5]), 3);
 	assert.equal(math.average([10, 20, 30, 40, 50]), 30);
+});
+
+suites['modulo/']('modulo numbers', () => {
+	assert.equal(math.modulo(10, 0), NaN);
+
+	assert.equal(math.modulo(10, 3), 1);
+	assert.equal(math.modulo(-10, 3), 2);
+	assert.equal(math.modulo(10, -3), -2);
+	assert.equal(math.modulo(-10, -3), -1);
+	assert.equal(math.modulo(10, 5), 0);
+	assert.equal(math.modulo(-10, 5), 0);
+	assert.equal(math.modulo(10, -5), 0);
+	assert.equal(math.modulo(-10, -5), 0);
+	assert.equal(math.modulo(10, 1), 0);
+	assert.equal(math.modulo(10, 10), 0);
 });
 
 Object.values(suites).forEach((v) => v.run());
