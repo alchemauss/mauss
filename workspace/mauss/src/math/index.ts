4a9bff6c398ab5f263aa537042a04ad909a21cbf
Ignatius Bagus <ignatius.mbs@gmail.com>
2024-08-09 15:03:12 +0700

    feat: add `modulo` function (#255)

--- workspace/mauss/src/math/index.ts
+++ workspace/mauss/src/math/index.ts
@@ -20,3 +20,19 @@ export function average(numbers: number[]): number {
 export function clamp(min: number, max: number) {
 	return (value: number) => Math.max(min, Math.min(value, max));
 }
+
+/**
+ * The `%` is a remainder operator, this function computes the modulo operation and ensures a non-negative number for a non-negative divisor.
+ *
+ * @example
+ * ```javascript
+ * // returns 1
+ * modulo(5, 2);
+ *
+ * // returns 1
+ * modulo(-3, 2);
+ * ```
+ */
+export function modulo(a: number, n: number): number {
+	return ((a % n) + n) % n;
+}
