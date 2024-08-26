79a2d2b7893c8a18bc850583ffcf07dea9c06280
Ignatius Bagus <ignatius.mbs@gmail.com>
2024-08-26 21:02:02 +0700

    move math readme

--- workspace/mauss/src/math/index.ts
+++ workspace/mauss/src/math/index.ts
@@ -1,5 +1,6 @@
 export { permutation } from './set/index.js';
 
+/** A higher-order function that returns a function to clamp a number between a minimum and maximum value */
 export function clamp(min: number, max: number) {
 	return (value: number) => Math.max(min, Math.min(value, max));
 }
