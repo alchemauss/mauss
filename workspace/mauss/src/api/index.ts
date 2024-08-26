ac95b2acb969bc41619139f3856b43b4bde5d15d
Ignatius Bagus <ignatius.mbs@gmail.com>
2024-08-26 21:01:30 +0700

    move api readme

--- workspace/mauss/src/api/index.ts
+++ workspace/mauss/src/api/index.ts
@@ -5,7 +5,7 @@ type HTTPMethods = 'GET' | 'POST' | 'PUT' | 'DELETE';
 
 export interface FetcherConfig {
 	/**
-	 * Prepares a `RequestInit` object to pass into fetch
+	 * Prepares an object to pass into fetch that is called before the request is sent, it receives the specified `method` and `to` url string, and optionally a `body`, `from` URL, and `headers` object. It should _prepare_ and return an object that satisfies the [`RequestInit`](https://developer.mozilla.org/en-US/docs/Web/API/Request/Request) interface
 	 * @param {RequestInit} opts request init options
 	 */
 	prepare?(opts: {
@@ -16,7 +16,7 @@ export interface FetcherConfig {
 		headers?: Record<string, string>;
 	}): RequestInit;
 	/**
-	 * Intercepts url before getting passed to fetch
+	 * Intercepts the `url` before the request passed to fetch
 	 * @param {string} path url received from all api methods
 	 */
 	intercept?(path: string): string;
@@ -25,13 +25,15 @@ export interface FetcherConfig {
 	 */
 	sweep?(exception: unknown): string;
 	/**
-	 * Transforms raw response to desired data structure
+	 * Transforms raw response to desired data structure, it receives the response and can return anything that will used as the `payload`
 	 * @param {Response} res response object from fetch
+	 * @default r.json().catch(() => ({}))
 	 */
 	transform?(res: Response): Promise<unknown>;
 	/**
-	 * Determines if fetcher should exit with an error
+	 * Determines if fetcher should exit with an error, this function is called after the response is transformed and receives a clone of the initial response and the `payload`
 	 * @param {Response} res response object from fetch
+	 * @returns `string` if the request was unsuccessful or anything falsy if it was successful
 	 */
 	exit?(res: Response, payload: any): AlsoPromise<string | false | Nullish>;
 }
@@ -42,6 +44,51 @@ export interface SendOptions {
 	using?: typeof fetch;
 }
 
+/**
+ * Fetcher factory to create a `send` function to make requests that abstracts the `fetch` API.
+ *
+ * @example
+ *
+ * ```javascript
+ * import { fetcher, type SendOptions } from 'mauss/api';
+ * const send = fetcher({
+ *   prepare({ method, to, body }) {
+ *     // ... do some checks or logging
+ *     return {
+ *       method: method || 'GET',
+ *       mode: 'cors',
+ *       credentials: 'include',
+ *       headers: {
+ *         'Content-Type': 'application/json',
+ *       },
+ *       body: JSON.stringify(body),
+ *     };
+ *   },
+ *   exit({ status }, payload) {
+ *     if (status >= 500) return 'ServerError';
+ *     if (status >= 400) return 'ClientError';
+ *     // void or non-string means successful
+ *   },
+ * });
+ * // use the `send` function above to make and export an abstraction
+ * export const API = {
+ *   // use getter to determine the template and infer the defined parameters
+ *   get 'session/:id'() {
+ *     // `tsf` function from 'mauss/std'
+ *     const render = tsf('https://auth.example/{id}/login');
+ *     return (params: Parameters<typeof render>[0], options: SendOptions = {}) => {
+ *       const target = send(render(params), options);
+ *       return {
+ *         // ... abstraction methods, for example
+ *         async post() {
+ *           return await target.post();
+ *         },
+ *       };
+ *     };
+ *   },
+ * };
+ * ```
+ */
 export function fetcher({
 	prepare = ({ method, body }) => ({ method, body: JSON.stringify(body) }),
 	intercept = (url) => url,
