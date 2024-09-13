68959e520fcc7939f8c6f67fbcd30dd834e81c9b
Ignatius Bagus <ignatius.mbs@gmail.com>
2024-09-13 14:53:58 +0700

    reexport

--- workspace/mauss/src/core/index.ts
+++ workspace/mauss/src/core/index.ts
@@ -2,6 +2,14 @@ export { curry, pipe } from './lambda/index.js';
 
 export { debounce, immediate, throttle } from './processor/index.js';
 
-export { capitalize, identical, inverse, regexp, scope, unique } from './standard/index.js';
+export {
+	capitalize,
+	convert,
+	identical,
+	inverse,
+	regexp,
+	scope,
+	unique,
+} from './standard/index.js';
 
 export { tsf } from './tsf/index.js';
