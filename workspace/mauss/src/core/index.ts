6cea39b0afd8ad22e6735d6245df120576c51794
Ignatius Bagus <ignatius.mbs@gmail.com>
2024-09-13 14:55:07 +0700

    fix: export `sides` function

--- workspace/mauss/src/core/index.ts
+++ workspace/mauss/src/core/index.ts
@@ -2,6 +2,6 @@ export { curry, pipe } from './lambda/index.js';
 
 export { debounce, immediate, throttle } from './processor/index.js';
 
-export { capitalize, identical, inverse, regexp, scope, unique } from './standard/index.js';
+export { capitalize, identical, inverse, regexp, scope, sides, unique } from './standard/index.js';
 
 export { tsf } from './tsf/index.js';
