4b7031a7158ddbb5c1e3e21c2e840dd8c0058041
Ignatius Bagus <ignatius.mbs@gmail.com>
2024-09-20 13:40:04 +0700

    feat: add `indent` function (#262)

--- workspace/mauss/src/core/standard/index.ts
+++ workspace/mauss/src/core/standard/index.ts
@@ -61,6 +61,20 @@ export function identical(x: unknown, y: unknown): boolean {
 }
 
 /**
+ * A function to work with template strings and removes indentation based on the first line. This is useful when the template string is written with indentation for better readability, but the indentation is not desired in the final output.
+ */
+export function indent(text: string) {
+	const lines = text.split(/\r?\n/).filter((l) => l.trim());
+	const depth = (/^\s*/.exec(lines[0]) || [''])[0].length;
+	return {
+		depth,
+		trim() {
+			return lines.map((l) => l.slice(depth)).join('\n');
+		},
+	};
+}
+
+/**
  * A function that accepts a function and returns the same function with the order of parameters reversed. This can be used in conjunction with `compare` methods to sort the items in ascending values.
  *
  * @param fn any function with one or more arguments
