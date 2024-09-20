b5a0278151a02ea1a382c900cf6975f5cad38542
Ignatius Bagus <ignatius.mbs@gmail.com>
2024-09-20 15:01:12 +0700

    add async debounce

--- workspace/mauss/src/promises/index.ts
+++ workspace/mauss/src/promises/index.ts
@@ -1,3 +1,19 @@
+import type { AnyFunction } from '../typings/helpers.js';
+
+const DURATION = 300;
+
+export function debounce<F extends AnyFunction>(fn: F, time = DURATION) {
+	let timeout: NodeJS.Timeout;
+	type Returned = Promise<ReturnType<F>>;
+	return async <A extends Parameters<F>>(...args: A): Returned => {
+		if (timeout) clearTimeout(timeout);
+		await new Promise((fulfil) => {
+			timeout = setTimeout(fulfil, time);
+		});
+		return fn(...args);
+	};
+}
+
 export async function pause(ms: number): Promise<string> {
 	return new Promise((fulfil) => setTimeout(fulfil, ms));
 }
